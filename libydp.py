864c346e75ec747f4b961c9845f3a1d1577f2e42
Jakub Wilk <jwilk@jwilk.net>
2008-07-30 13:45:10 +0200

    libydp: use `libydpdict.so.2`.

--- libydp.py
+++ libydp.py
@@ -1,12 +1,21 @@
 __all__ = ('YdpDict',)
 
 from ctypes import CDLL, Structure as CStructure
-from ctypes import c_void_p, c_uint16, c_uint32, c_int, c_char, c_char_p
+from ctypes import c_void_p, c_uint16, c_int, c_char, c_char_p
 from ctypes import POINTER as c_pointer_t, pointer as c_pointer, cast
 from ctypes import pythonapi as libpython, py_object
 from lxml.etree import HTMLParser, HTML
 
-liby = CDLL('libydpdict.so.1')
+libydp = CDLL('libydpdict.so.2')
+ydp_read_xhtml = libydp.ydpdict_read_xhtml
+ydp_read_xhtml.restype = c_pointer_t(c_char)
+ydp_open = libydp.ydpdict_open
+ydp_open.restype = c_void_p
+ydp_get_word = libydp.ydpdict_get_word
+ydp_get_word.restype = c_char_p
+ydp_close = libydp.ydpdict_close
+ydp_get_count = libydp.ydpdict_get_count
+
 libc = CDLL('libc.so.6')
 
 html_parser = HTMLParser(recover = False, no_network = True)
@@ -18,13 +27,11 @@ class YdpWord(object):
 	
 	@property
 	def name(self):
-		return self.owner.words[self.nth].decode('UTF-8')
+		return self.owner._get_word(self.nth).decode('UTF-8')
 	
 	@property
 	def definition(self):
-		read_xhtml = liby.ydpdict_read_xhtml
-		read_xhtml.restype = c_pointer_t(c_char)
-		result = read_xhtml(c_pointer(self.owner), c_uint32(self.nth))
+		result = ydp_read_xhtml(self.owner._pointer, c_int(self.nth))
 		if result is None:
 			raise libpython.PyErr_SetFromErrno(py_object(OSError))
 		try:
@@ -32,27 +39,18 @@ class YdpWord(object):
 		finally:
 			libc.free(result)
 
-class YdpDict(CStructure):
-	_fields_ = \
-	(
-		('dat', c_void_p),
-		('idx', c_void_p),
-		('words', c_pointer_t(c_char_p)),
-		('word_count', c_uint16),
-		('indices', c_void_p),
-		('encoding', c_int),
-		('xhtml_header', c_int),
-		('xhtml_title', c_char_p),
-		('xhtml_style', c_char_p),
-		('xhtml_use_style', c_int)
-	)
+class YdpDict(object):
+
+	def _get_word(self, i):
+		return ydp_get_word(self._pointer, c_int(i))
 
 	def __init__(self, dat_file_name, idx_file_name):
-		rv = liby.ydpdict_open(c_pointer(self), dat_file_name, idx_file_name, 1)
-		if rv != 0:
+		self._pointer = ydp_open(dat_file_name, idx_file_name, 1)
+		if not self._pointer:
 			self._open = False
 			raise libpython.PyErr_SetFromErrno(py_object(OSError))
 		self._open = True
+		self._word_count = ydp_get_count(self._pointer)
 
 	def __enter__(self):
 		return self
@@ -61,13 +59,13 @@ class YdpDict(CStructure):
 		self.close()
 
 	def __iter__(self):
-		return (self[i] for i in xrange(self.word_count))
+		return (self[i] for i in xrange(self._word_count))
 
 	def __getitem__(self, nth):
 		return YdpWord(self, nth)
 			
 	def close(self):
 		if self._open:
-			liby.ydpdict_close(c_pointer(self))
+			ydp_close(self._pointer)
 
 # vim:ts=4 sw=4 noet
