jwilk-archive/ydpy

commit 61e82605f0313818ef273c54188d40e922d300ce

Remove distutils-based build system.

.travis.yml
@@ -9,7 +9,6 @@ python:
 - "3.6-dev"
 - "3.7-dev"
 script:
-- python setup.py build
-- python -m compileall -f build
+- python -m compileall -f .
 
 # vim:ts=2 sts=2 sw=2 et

MANIFEST.in
@@ -1,3 +1,0 @@
-include MANIFEST.in
-include doc/changelog
-include ydpy.py

setup.py
@@ -1,71 +1,0 @@
-# encoding=UTF-8
-
-# Copyright © 2010-2016 Jakub Wilk <jwilk@jwilk.net>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the “Software”), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-'''
-*ydpy* is a command-line interface to the Collins English-Polish/Polish-English
-and/or Langenscheidt German-Polish/Polish-German dictionaries distributed by
-`Young Digital Planet`_.
-
-.. _Young Digital Planet:
-   http://www.ydp.pl/
-'''
-
-classifiers = '''
-Development Status :: 4 - Beta
-Environment :: Console
-Intended Audience :: End Users/Desktop
-License :: OSI Approved :: MIT License
-Natural Language :: Polish
-Operating System :: POSIX
-Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 3
-Topic :: Text Processing :: Linguistic
-'''.strip().splitlines()
-
-import distutils.core
-import distutils.command.build_py
-
-from lib import version
-
-try:
-    build_py = distutils.command.build_py.build_py_2to3
-except AttributeError:
-    build_py = distutils.command.build_py.build_py
-
-distutils.core.setup(
-    name='ydpy',
-    version=version.__version__,
-    license='MIT',
-    description='command-line interface for Collins and Langenscheidt dictionaries',
-    long_description=__doc__.strip(),
-    classifiers=classifiers,
-    url='http://jwilk.net/software/ydpy',
-    author='Jakub Wilk',
-    author_email='jwilk@jwilk.net',
-    packages=['ydpy'],
-    package_dir=dict(ydpy='lib'),
-    scripts=['ydpy'],
-    cmdclass=dict(build_py=build_py)
-)
-
-# vim:ts=4 sts=4 sw=4 et

commit e6943fc1884cabb4a4c1b2d82de26ffb6c7891bb

Port to Python 3.X.
Python 3.X compatibility was broken in
61e82605f0313818ef273c54188d40e922d300ce.

This patch restores the compatibility, and drops support for Python 2.X
at the same time.

.travis.yml
@@ -1,7 +1,5 @@
 language: python
 python:
-- "2.6"
-- "2.7"
 - "3.2"
 - "3.3"
 - "3.4"

lib/__init__.py
@@ -22,7 +22,7 @@
 
 import sys
 
-if sys.version_info < (2, 6):
-    raise RuntimeError('Python >= 2.6 is required')
+if sys.version_info < (3, 2):
+    raise RuntimeError('Python >= 3.2 is required')
 
 # vim:ts=4 sts=4 sw=4 et

lib/cli.py
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import print_function
-
 import argparse
 import errno
 import locale
@@ -72,28 +70,16 @@ class ArgumentParser(argparse.ArgumentParser):
         self.add_argument('term', metavar='SEARCH-TERM', nargs='?')
         self.set_defaults(dict_n=100)
 
-if str is bytes:
-    # Python 2
-    def write_bytes(file, bytestring):
-        file.write(bytestring)
-else:
-    # Python 3
-    def write_bytes(file, bytestring):
-        file.flush()
-        file.buffer.write(bytestring)
-
 def main():
     options = ArgumentParser().parse_args()
     encoding = locale.getpreferredencoding()
     if options.term:
         term = options.term
-        if not isinstance(term, unicode):
-            term = term.decode(encoding)
         matcher = re.compile(term, re.UNICODE | re.IGNORECASE | re.DOTALL).search
     else:
         matcher = id
     path = options.path or config.get('Path') or '/usr/share/ydpdict'
-    if isinstance(path, unicode):
+    if isinstance(path, str):
         path = path.encode(sys.getfilesystemencoding())
     dict_n = options.dict_n
     if sys.stdout.isatty():
@@ -113,7 +99,8 @@ def main():
                 formatter = YdpFormatter(encoding=encoding)
                 formatter(entry.definition)
                 bytestring = formatter.encode()
-                write_bytes(sys.stdout, bytestring)
+                sys.stdout.flush()
+                sys.stdout.buffer.write(bytestring)
     finally:
         if formatter:
             print(formatter.cleanup())

lib/format_text.py
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 import collections
-from StringIO import StringIO
+import io
 
 from . import transliterate
 del transliterate  # Hi, pyflakes!
@@ -73,10 +73,10 @@ class YdpFormatter(object):
 
     def parse_div(self, node):
         tmp_file = self._file
-        self._file = StringIO()
+        self._file = io.StringIO()
         for subnode in node:
             self(subnode)
-        result = unicode(self)
+        result = str(self)
         self._file = tmp_file
         self.write('\n  ')
         self.write(result.replace('\n', '\n  '))
@@ -116,25 +116,16 @@ class YdpFormatter(object):
         return ''.encode()
 
     def __init__(self, encoding):
-        self._file = StringIO()
+        self._file = io.StringIO()
         self._strip = False
         self._color_map = collections.defaultdict(str)
         self._encoding = encoding
 
-    if str is bytes:
-        # Python 2
-        def __unicode__(self):
-            return self._file.getvalue()
-        def __str__(self):
-            return unicode(self).encode(self._encoding, 'transliterate')
-        def encode(self):
-            return str(self)
-    else:
-        # Python 3
-        def __str__(self):
-            return self._file.getvalue()
-        def encode(self):
-            return str(self).encode(self._encoding, 'transliterate')
+    def __str__(self):
+        return self._file.getvalue()
+
+    def encode(self):
+        return str(self).encode(self._encoding, 'transliterate')
 
     def __call__(self, node):
         if node.tag.isalpha():

lib/libydp.py
@@ -79,7 +79,7 @@ class YdpDict(object):
         self.close()
 
     def __iter__(self):
-        return (self[i] for i in xrange(self._word_count))
+        return (self[i] for i in range(self._word_count))
 
     def __getitem__(self, nth):
         return YdpWord(self, nth)

lib/transliterate.py
@@ -21,71 +21,71 @@
 # SOFTWARE.
 
 FALLBACK = {
-    u'\xa0': '\x20',
-    u'°': '^o',
-    u'·': '.',
-    u'Ä': 'A',
-    u'Ó': 'O',
-    u'Ö': 'O',
-    u'Ü': 'U',
-    u'ß': 'ss',
-    u'á': 'a',
-    u'â': 'a',
-    u'ä': 'a',
-    u'æ': '<ae>',
-    u'ç': 'c',
-    u'é': 'e',
-    u'ë': 'e',
-    u'í': 'i',
-    u'ï': 'i',
-    u'ð': '<6>',
-    u'ó': 'o',
-    u'ô': 'o',
-    u'ö': 'o',
-    u'ü': 'u',
-    u'ă': 'a',
-    u'ą': 'a',
-    u'Ć': 'C',
-    u'ć': 'c',
-    u'č': 'c',
-    u'Ę': 'E',
-    u'ę': 'e',
-    u'Ł': 'L',
-    u'ł': 'l',
-    u'ń': 'n',
-    u'ŋ': '<n>',
-    u'ŕ': 'r',
-    u'Ś': 'S',
-    u'ś': 's',
-    u'Ź': 'Z',
-    u'ź': 'z',
-    u'Ż': 'Z',
-    u'ż': 'z',
-    u'ɑ': '<a>',
-    u'ɔ': '<o>',
-    u'ə': '<e>',
-    u'ɛ': '<E>',
-    u'ɪ': '<i>',
-    u'ʃ': '<|>',
-    u'ʌ': '<^>',
-    u'ʒ': '<3>',
-    u'ˈ': "'",
-    u'ː': ':',
-    u'θ': '<0>',
-    u'–': '--',
-    u'‘': "`",
-    u'’': "'",
-    u'”': "''",
-    u'„': ',,',
-    u'†': '<t>',
-    u'‡': '<tt>',
-    u'…': '...',
+    '\xa0': '\x20',
+    '°': '^o',
+    '·': '.',
+    'Ä': 'A',
+    'Ó': 'O',
+    'Ö': 'O',
+    'Ü': 'U',
+    'ß': 'ss',
+    'á': 'a',
+    'â': 'a',
+    'ä': 'a',
+    'æ': '<ae>',
+    'ç': 'c',
+    'é': 'e',
+    'ë': 'e',
+    'í': 'i',
+    'ï': 'i',
+    'ð': '<6>',
+    'ó': 'o',
+    'ô': 'o',
+    'ö': 'o',
+    'ü': 'u',
+    'ă': 'a',
+    'ą': 'a',
+    'Ć': 'C',
+    'ć': 'c',
+    'č': 'c',
+    'Ę': 'E',
+    'ę': 'e',
+    'Ł': 'L',
+    'ł': 'l',
+    'ń': 'n',
+    'ŋ': '<n>',
+    'ŕ': 'r',
+    'Ś': 'S',
+    'ś': 's',
+    'Ź': 'Z',
+    'ź': 'z',
+    'Ż': 'Z',
+    'ż': 'z',
+    'ɑ': '<a>',
+    'ɔ': '<o>',
+    'ə': '<e>',
+    'ɛ': '<E>',
+    'ɪ': '<i>',
+    'ʃ': '<|>',
+    'ʌ': '<^>',
+    'ʒ': '<3>',
+    'ˈ': "'",
+    'ː': ':',
+    'θ': '<0>',
+    '–': '--',
+    '‘': "`",
+    '’': "'",
+    '”': "''",
+    '„': ',,',
+    '†': '<t>',
+    '‡': '<tt>',
+    '…': '...',
 }
 
 def handler(exception):
     if isinstance(exception, (UnicodeEncodeError, UnicodeTranslateError)):
-        s = u''.join(
-            FALLBACK.get(ch, u'<?>')
+        s = ''.join(
+            FALLBACK.get(ch, '<?>')
             for ch in exception.object[exception.start:exception.end]
         )
         return s, exception.end

ydpy
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding=UTF-8
 
 # Copyright © 2007-2016 Jakub Wilk <jwilk@jwilk.net>