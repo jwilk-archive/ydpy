jwilk-archive/ydpy

commit c24b24ba49ed81fa227b86663d6081e149974147

Makefile: install manpage only if it exists.
The manual page doesn't exist in a pristine git checkout,
but it does exist in a release tarball.

Makefile
@@ -46,8 +46,12 @@ install:
 	# library:
 	( find lib -type f ! -name '*.py[co]' ) \
 	| xargs -t -I {} $(INSTALL) -p -D -m644 {} $(DESTDIR)$(basedir)/{}
+ifeq "$(wildcard .git doc/$(exe).1)" ".git"
+	# run "make -C doc" to build the manpage
+else
 	# manual page:
 	$(INSTALL) -p -D -m644 doc/$(exe).1 $(DESTDIR)$(mandir)/man1/$(exe).1
+endif
 
 .PHONY: clean
 clean:

commit 4ff4e394dcfbbe6c442472d0c2eb8bea9315d5a2

Makefile: don't check for .git existence.
Some users might use a git snapshot that doesn't have the .git directory
(for example, an hg-git clone, or a tarball automatically generated by
GitHub).

Fixes: c24b24ba49ed81fa227b86663d6081e149974147

Makefile
@@ -46,7 +46,7 @@ install:
 	# library:
 	( find lib -type f ! -name '*.py[co]' ) \
 	| xargs -t -I {} $(INSTALL) -p -D -m644 {} $(DESTDIR)$(basedir)/{}
-ifeq "$(wildcard .git doc/$(exe).1)" ".git"
+ifeq "$(wildcard doc/$(exe).1)" ""
 	# run "$(MAKE) -C doc" to build the manpage
 else
 	# manual page: